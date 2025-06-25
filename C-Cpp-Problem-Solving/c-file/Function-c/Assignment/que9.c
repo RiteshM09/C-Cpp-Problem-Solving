b3709a3ab8291616901549dca875c0f6d7428dfe
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-06-26 03:56:27 +0530

    add que10.c solution

--- C-Cpp-Problem-Solving/c-file/Function-c/Assignment/que9.c
+++ C-Cpp-Problem-Solving/c-file/Function-c/Assignment/que9.c
@@ -1,49 +1,52 @@
 // Write a function to calculate number of permutations
 //  which can be made from n items, selected r at a time. (TSRS)
 // formula n! /(n-r)!
-
-// condition (n>r)
+// special cases - when n>r | n,0 = 1 | n,1 = n | n,n = n!
 // ex:
 // n=3 , r=2
 // A,B,C
-// AB BC AC CA CB BA 
+// AB BC AC CA CB BA
 
 // n!/(n-r)!
-#include<stdio.h>
-int Permutation(int,int);
+#include <stdio.h>
+int Permutation(int, int);
 int main()
 {
-    int n,r,Result;
+    int n, r, Result;
     printf("Enter number of item  and it's arragement n = ");
     scanf("%d", &n);
 
     printf("r = ");
     scanf("%d", &r);
 
-    if(n>r){
-        Result=Permutation(n,r); // actual argument 
-        printf(" Number of Permutation are = %d",Result);
+    if (n >= r)     // here 2 special cases are handled n>r and n=r
+    {
+        Result = Permutation(n, r); // actual argument
+        printf(" Number of Permutation are = %d", Result);
     }
-    else{
+    else
+    {
         printf("Invalid Inputs i.e n>r ");
     }
 
     return 0;
 }
-int Permutation(int x,int y){
+int Permutation(int x, int y)
+{
 
-    int f=1,i,temp=1,z;
+    int f = 1, i, temp = 1, z;
 
-    for(i=1;i<=x-y;i++)
+    for (i = 1; i <= x - y; i++)
     {
-        temp=temp*i;
+        temp = temp * i;
     }
 
-    for(i=1;i<=x;i++){ 
-        
-        f=f*i;
+    for (i = 1; i <= x; i++)
+    {
+        f = f * i;
     }
 
-    z=f/temp; 
+    z = f / temp;
     return z;
 }
+
