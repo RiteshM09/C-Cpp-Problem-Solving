ab7d84beffc031869550e8d527199a5d0dd9295f
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-06-27 02:00:00 +0530

    added prime no. sol

--- C-Cpp-Problem-Solving/c-file/Function-c/Assignment/que11.c
+++ C-Cpp-Problem-Solving/c-file/Function-c/Assignment/que11.c
@@ -3,30 +3,38 @@
 // A prime number is a number greater than 1 that has only two factors: 1 and itself.
 // ex : // 2 3 5 7 11 13 17 19 23 29 31 37
 
-int Prime(int);
+
 #include<stdio.h>
+
+int Prime(int);
 int main()
 {
-    int n;
+    int n, result;
     printf("Enter a nuber = ");
     scanf("%d",&n);
-    Prime(n);
-    
-    retrun 0;
+    result=Prime(n);
+    if(result==1)
+        printf("\n Prime ");
+    else 
+        printf("Not a prime");
+
+    return 0;
 }
 int Prime(int x){
-    int i=2;
+    int i=2,temp=0;
     
-    if(n>1){
-        while(x%x==0){           
-            
+    if(x>=i){
+        for(i=2;x/2;i++){  
+        if(x%i==0){ 
+          
+        temp=1;
         }
     }
-    else{
-        printf("Not prime");
     }
-
-
+    if(temp==0)
+        return 1; 
+    else
+     return 0;
 }
 // 1 2 3 4 5 6 7 8 9 10 11 12
 
