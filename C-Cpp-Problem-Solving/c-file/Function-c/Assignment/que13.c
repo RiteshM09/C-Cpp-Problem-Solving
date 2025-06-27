c79086cb4ccff1cf5ff65822763306310c78bd56
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-06-28 03:21:04 +0530

    git added fuction sol13

---
+++ C-Cpp-Problem-Solving/c-file/Function-c/Assignment/que13.c
@@ -0,0 +1,40 @@
+//Write a function to print all prime numbers between two given numbers. (TSRN)
+//ex:
+// 1 10
+// 2 3 5 7 
+
+// 4 2
+// 5 7 11 13 17 19
+
+#include<stdio.h>
+void PrintPrime(int,int);
+int main(){
+    int a,b;
+    printf("Enter starting no. = ");
+    scanf("%d",&a);
+    printf("last no. =");
+    scanf("%d",&b);
+    PrintPrime(a,b);
+
+    return 0;
+}
+
+void PrintPrime(int x ,int y)
+{
+    int i,temp;
+for (size_t i = 0; i < count; i++)
+{
+    /* code */
+}
+
+    for(i=x;i<=y-1;i++){   
+        
+        if(y%i==0)
+          
+    
+        if(temp>1)
+             printf("%d ",i); 
+        
+    }
+
+}
