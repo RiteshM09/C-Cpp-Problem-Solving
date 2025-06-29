8f7a1687051f117bad99f4f706e1116aee13ed88
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-06-30 04:08:43 +0530

    adde 14 que solving approch

--- C-Cpp-Problem-Solving/c-file/Function-c/Assignment/que14.c
+++ C-Cpp-Problem-Solving/c-file/Function-c/Assignment/que14.c
@@ -1,1 +1,32 @@
 //Write a function to check whether a given number is an Armstrong number or not.
+// function            
+// Armstrong Number 
+// ex: 1 to 9
+// ex: 153 
+// 1^3+5^3+3^3 - 1+125+27 = 153
+// 9474
+// 9^4 + 9^4 + 4^4 + 7^4  = 6561 + 256 + 2401 + 256 = 947
+
+// 153
+// 1^3 + 5^3 + 3^3
+// 100*1 + 10*5*5*5 + 3*3*3
+153 15 3
+
+temp=n/10    temp=15 n=153
+n=temp%10 temp=15, n=3
+
+while(n){153
+    temp=n/10    temp=15, n=153
+    n=temp%10    temp=15, n=3
+    i++;
+}
+
+int i,temp;
+for(;n>0;i++){
+     n=n/10; 
+}
+i=3
+
+
+
+count=3;
