6b3e0e0978ad6475f20f0bd727b505e092262905
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-07-01 03:41:12 +0530

    updated sol of que 17 function

--- C-Cpp-Problem-Solving/c-file/Function-c/Assignment/que17.c
+++ C-Cpp-Problem-Solving/c-file/Function-c/Assignment/que17.c
@@ -1,29 +1,24 @@
 //Write a function to Reverse a number
-void ReverseN(int);
+// 
+int ReverseN(int);
 #include<stdio.h>
 int main()
 {
     int N,Result;
     printf("Enter a Number = ");
     scanf("%d",&N);
-    ReverseN(N);
+    Result=ReverseN(N);
+    printf("Reverse of N = %d ",Result);
     return 0;
 }
-void ReverseN(int n){
-    int temp,count=0;
-    temp=n;
-    while(temp){
-        temp=temp/10;
-        count++;
-    }
-    
-    printf("Reverse of N = ");
-    for(int i=1,temp=n;i<=count;i++){
-        temp=n%10; 
-        printf("%d",temp);
-        n=n/10; 
+int ReverseN(int n){
+    int rev=0;
+
+    while(n){
+    rev=10*rev+(n%10);
+    n=n/10;
     }
 
+    return rev;
 }
-
 
