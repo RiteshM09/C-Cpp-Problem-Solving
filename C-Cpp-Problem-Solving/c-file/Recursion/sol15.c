6913fc7bb134fd351c3f0d87caf62b60df59b5e6
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-07-03 18:03:32 +0530

    sol15 fibonacci series added

---
+++ C-Cpp-Problem-Solving/c-file/Recursion/sol15.c
@@ -0,0 +1,39 @@
+//Write a recursive function to find nt term of a Fibonacci series
+
+// function             -      fib(n) -  0112358..n 
+// R.C                  -    fib(n-1) - (0112358)(print(fib n))
+                        
+// R.C                  -          FibonacciN(n-1) -  (0 1 1 2)+1    (0 1 1 2 3)+2
+                                   
+//fibonacci series 
+// for n= 0 1 2 3 4 5 6 7 8  9  10 11 12  13....
+//term    0 1 1 2 3 5 8 13 21 34 55 89 144....
+#include <stdio.h>
+int Fib(int); 
+void PrintFib(int);
+ 
+int main() {
+    int N;
+
+    scanf("%d", &N);
+    PrintFib(N);
+}
+
+int Fib(int n){
+    if(n==0 || n==1)
+        return n;
+    return Fib(n-1)+Fib(n-2);
+}
+
+void PrintFib(int n){
+    if(n==0)
+        printf("%d ", n);
+    else{
+        PrintFib(n-1);
+        printf("%d ",Fib(n));
+    }
+
+}
+//  fib(7) -  0112358 13 
+//  fib(6) - (0112358)(print(fib 7)) fib(6)+(n-2)+(n-1)
+
