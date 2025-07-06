99728f51505a7c3c8ffcbf9319fcb9025a5cc74d
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-07-07 02:08:00 +0530

    sol2 start patter added and sol1 updated

--- C-Cpp-Problem-Solving/c-file/Star-Pattern/sol1.c
+++ C-Cpp-Problem-Solving/c-file/Star-Pattern/sol1.c
@@ -1,16 +1,16 @@
 /*
 
-****    i=1 ,j=4 - j>=5-i    
-***     i=2 ,j=3 - j=5-i
-**      i=3 ,j=2 - j=5-i
-*       i=4 ,j=1 - j=5-i
+****    i=1 ,j=4 - j<=5-i    
+***     i=2 ,j=3 - j<=5-i
+**      i=3 ,j=2 - j<=5-i
+*       i=4 ,j=1 - j<=5-i
 
 */
 
 
 #include<stdio.h>
 int main()
-{   int i,j;
+{  
 
     for(int i=1;i<=4;i++){
         for(int j=1;j<=5-i;j++){
@@ -19,8 +19,6 @@ int main()
             
         }
          printf("\n");    
-
-        
     }
 
     return 0;
