a7a0f3063231c813e1fb5b4880a9a66955c50dbd
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-07-07 03:02:44 +0530

    add start patter sol5

---
+++ C-Cpp-Problem-Solving/c-file/Star-Pattern/sol5.c
@@ -0,0 +1,25 @@
+/*
+    *           i=4 j=4             j>=4 & j<=4  j>=5-1 &  j<=3+i 
+   ***          i=3 j=345           j>=3 & j<=5  j>=5-2 &  j<=3+i
+  *****         i=2 j=23456         j>=2 & j<=6  j>=5-3 &  j<=3+i
+ *******        i=1 j=1234567       j>=1 & j<=7  j>=5-4 &  j<=3+i
+*/
+#include<stdio.h>
+int main()
+{
+    int i,j;
+
+    for(i=1;i<=4;i++)
+    {
+        for(j=1;j<=3+i;j++)
+        {
+            if(j>=5-i)
+                printf("*");
+            else    
+                printf(" ");
+            
+        }
+        printf("\n");
+    }
+    return 0;
+}
