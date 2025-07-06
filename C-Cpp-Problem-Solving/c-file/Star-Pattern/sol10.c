b55b2dbf5fd7122efab0dd8b9c68bcb4407ab441
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-07-07 03:31:31 +0530

    sol 10 updated

--- C-Cpp-Problem-Solving/c-file/Star-Pattern/sol10.c
+++ C-Cpp-Problem-Solving/c-file/Star-Pattern/sol10.c
@@ -16,15 +16,11 @@ int main()
     {
         for(j=1;j<=5;j++)
         {
-            if(i==1 || i==5){
+            if(i==1 || i==5 || j==1 || j==5)
                 printf("*");
-            } 
-            else  { 
-                if(j<=1 && j>=5)
-                    printf("*");
-                else
-                    printf(" ");
-            }
+            else
+                printf(" ");
+            
         }
         printf("\n");
     }
