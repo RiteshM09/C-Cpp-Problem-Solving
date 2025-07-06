99f24007f1d82e225484ba302075e75887a6d485
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-07-06 17:09:12 +0530

    added start pattern

---
+++ C-Cpp-Problem-Solving/c-file/Star-Pattern/sol1.c
@@ -0,0 +1,27 @@
+/*
+
+****    i=1 ,j=4 - j>=5-i    
+***     i=2 ,j=3 - j=5-i
+**      i=3 ,j=2 - j=5-i
+*       i=4 ,j=1 - j=5-i
+
+*/
+
+
+#include<stdio.h>
+int main()
+{   int i,j;
+
+    for(int i=1;i<=4;i++){
+        for(int j=1;j<=5-i;j++){
+            // if(j<=5-i)
+            printf("* ");
+            
+        }
+         printf("\n");    
+
+        
+    }
+
+    return 0;
+}
