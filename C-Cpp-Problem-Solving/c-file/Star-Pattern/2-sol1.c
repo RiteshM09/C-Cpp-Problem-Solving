8460c3c1c8f05525fe9b40555d05d8e3a3b8e9ff
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-07-07 23:48:05 +0530

    start p Ass2 sol1 added

---
+++ C-Cpp-Problem-Solving/c-file/Star-Pattern/2-sol1.c
@@ -0,0 +1,27 @@
+/*
+
+*       i=1 ,j=1 
+**      i=2 ,j=2 
+***     i=3 ,j=3
+****    i=4 ,j=4
+*****   i=5 ,j=5
+
+*/
+
+
+#include<stdio.h>
+int main()
+{  
+
+    for(int i=1;i<=5;i++){
+
+        for(int j=1;j<=5;j++){
+            if(j<=i)
+            printf("* ");
+            
+        }
+         printf("\n");    
+    }
+
+    return 0;
+}
