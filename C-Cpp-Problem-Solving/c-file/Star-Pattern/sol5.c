6339ef2d4f0c574def5d3d91aacfadc23de10112
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-07-08 16:55:18 +0530

    code upadated

--- C-Cpp-Problem-Solving/c-file/Star-Pattern/sol5.c
+++ C-Cpp-Problem-Solving/c-file/Star-Pattern/sol5.c
@@ -11,9 +11,9 @@ int main()
 
     for(i=1;i<=4;i++)
     {
-        for(j=1;j<=3+i;j++)
+        for(j=1;j<=7;j++)
         {
-            if(j>=5-i)
+            if(j<=3+i && j>=5-i)
                 printf("*");
             else    
                 printf(" ");
