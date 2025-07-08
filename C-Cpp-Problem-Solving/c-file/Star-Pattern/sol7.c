37246345435de8ea0f78481c700b2f2e0a3635f6
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-07-09 02:32:42 +0530

    sol 7 star patter added of Assign 1

--- C-Cpp-Problem-Solving/c-file/Star-Pattern/sol7.c
+++ C-Cpp-Problem-Solving/c-file/Star-Pattern/sol7.c
@@ -5,7 +5,14 @@ AB   BA
 A     A
 */
 
-// 1234321 i=1 , j<=4  or j>=4  
+// j values,
+// 1234567
+// 123 567
+// 12   67
+// 1     7
+
+// k values,
+// 1234321 i=1 , j<=4  or j>=4  j<=5-i or j>=3+i
 // 123_321 i=2 , j<=3  or j>=5  
 // 12___21 i=3 , j<=2  or j>=6  
 // 1_____1 i=4 , j<=1  or j>=7  
@@ -14,24 +21,28 @@ A     A
 #include<stdio.h>
 int main()
 {
-    int i,j,k;
+    int i,j,ch;
   
     for(i=1;i<=4;i++)
     {
        
-        for(j=1,k=1;j<=7;j++)  
+        for(j=1,ch='A';j<=7;j++)  
         {
             
             if(j<=5-i || j>=3+i){
-                printf("%d",k); 
+                printf("%c",ch); 
 
                 if(j<4)
-                    k++;
+                    ch++;
                 else    
-                    k--;
+                    ch--;
             }
-            else
+            else{
+                if(j==4)
+                    ch--;
                 printf(" ");
+            }
+                
             } 
         printf("\n");
     }
