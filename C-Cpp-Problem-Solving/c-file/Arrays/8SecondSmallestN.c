05d5bb941afe53e143c840efebceeed15178b1b0
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-07-11 18:23:12 +0530

    adde 8SecondSmallestN.c

---
+++ C-Cpp-Problem-Solving/c-file/Arrays/8SecondSmallestN.c
@@ -0,0 +1,12 @@
+// Write a program to find the second smallest number in an array. Take array values from the user.
+#include<stdio.h>
+int main()
+{
+    int a[10],i,min, second_min;
+
+    printf("Enter 10 Values :");
+    for(i=0;i<=9;i++)
+        scanf("%d",&a[i]);
+    
+    return 0;
+}
