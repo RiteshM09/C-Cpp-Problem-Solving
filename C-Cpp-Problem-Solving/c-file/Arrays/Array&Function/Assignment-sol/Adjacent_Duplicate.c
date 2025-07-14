219cf5aedaa0f6bbfada7392fe9062ad9c6b009a
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-07-14 18:15:49 +0530

    naming

---
+++ C-Cpp-Problem-Solving/c-file/Arrays/Array&Function/Assignment-sol/Adjacent_Duplicate.c
@@ -0,0 +1,5 @@
+// Write a function to find the first occurrence of adjacent duplicate values in the array.
+// Function has to return the value of the element.
+// input - 12 22 32 22 45 45 11 44 11 11
+// output- fist adjacent duplicate is 45, return 45
+
