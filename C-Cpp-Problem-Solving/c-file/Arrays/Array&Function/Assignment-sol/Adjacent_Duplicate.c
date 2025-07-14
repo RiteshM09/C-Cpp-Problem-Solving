c22505cd7a30d88a71ab0c546978d683a2cf12f6
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-07-14 19:15:16 +0530

    sol of find first occurrence of  Adjacent_Duplicate

--- C-Cpp-Problem-Solving/c-file/Arrays/Array&Function/Assignment-sol/Adjacent_Duplicate.c
+++ C-Cpp-Problem-Solving/c-file/Arrays/Array&Function/Assignment-sol/Adjacent_Duplicate.c
@@ -2,4 +2,31 @@
 // Function has to return the value of the element.
 // input - 12 22 32 22 45 45 11 44 11 11
 // output- fist adjacent duplicate is 45, return 45
+#include<stdio.h>
+int Adjacent_duplicate(int A[],int);
+int main()
+{
+    int s,a[20],i,Result;
+    printf("Enter Array Size :");
+    scanf("%d",&s);
 
+    printf("Enter Array Element :");
+    for(i=0;i<s;i++)
+        scanf("%d",&a[i]);
+    Result=Adjacent_duplicate(a,s);
+    if(Result == 0)
+        printf("NO duplicate found");
+    else
+        printf("First found Adjacent Duplicate value is: %d ",Result);
+
+}
+int Adjacent_duplicate(int A[],int S){
+    for(int i=1;i<S;i++)  
+    {                      
+            if(A[i]==A[i+1]) {
+                return A[i];
+                break;
+            }
+    }
+    return 0;
+}
