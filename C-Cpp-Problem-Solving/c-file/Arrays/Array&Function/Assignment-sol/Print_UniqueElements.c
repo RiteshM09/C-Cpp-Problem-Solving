94ed18ca911360c16cee68a19bc5d4277db68f2c
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-07-15 18:29:09 +0530

    new Que Print_UniqueElements in an array added

---
+++ C-Cpp-Problem-Solving/c-file/Arrays/Array&Function/Assignment-sol/Print_UniqueElements.c
@@ -0,0 +1,10 @@
+// Write a function to print all unique elements in an array.
+// input : 10 20 10 20 30 40 20 30
+// output: 10 10 20 20 20 30 30 40
+
+// output: 40
+void Print_unique(int A[],int size){
+    int i;
+    
+    
+}
