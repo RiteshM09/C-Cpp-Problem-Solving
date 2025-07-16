2c14c2b10aea02ab8f5803b2ee0c9e067a4ed71c
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-07-16 20:33:06 +0530

    MERGE 2 sorted array in decending order in 3rd array

--- C-Cpp-Problem-Solving/c-file/Arrays/Array&Function/Assignment-sol/Merge_Array.c
+++ C-Cpp-Problem-Solving/c-file/Arrays/Array&Function/Assignment-sol/Merge_Array.c
@@ -1,13 +1,15 @@
 // Write a function to merge two arrays of the same size sorted in descending order.
-// Input:3 4 
+// using third array.
+// Input: 4
 // Array 1: 5 3 2 1 
 // Array 2: 9 8 7 2
 // Merge array: 9 8 7 5 3 2 2 1
+//
 #include<stdio.h>
-void MergeArray(int A[],int B[],int,int merge[]);
+void MergeArray(int A[],int B[],int merge[],int);
 int main()
 {
-    int a[20],b[20],i,j,s,m[2*s];
+    int a[20],b[20],i,j,s,m[40];
     printf("Enter array size:");
     scanf("%d", &s);
     
@@ -18,17 +20,18 @@ int main()
     printf("\nEnter 2 array Element :");
     for(j=0;j<s;j++)
         scanf("%d", &b[j]);
-
-    MergeArray(a,b,s,m);
+    printf("\n");
+    
+    MergeArray(a,b,m,s);
 
     return 0;
 }
-void MergeArray(int A[],int B[],int size,int merge[])
+void MergeArray(int A[],int B[],int merge[],int size)
 {
-    int i,j,r,temp;
+    int i,j,k,r,temp;
     // sort Array 1 in Decending Order
-    for(r=0;r<size;i++){
-        for(i=0;i<size-1-r;i++)
+    for(r=0;r<size;r++){
+        for(i=0;i<size-1-r;i++) 
         {
             if(A[i]<A[i+1]){
                 temp=A[i];
@@ -40,11 +43,11 @@ void MergeArray(int A[],int B[],int size,int merge[])
     }
 
     printf("Array 1 in Decending order:");
-    for(j=0;j<size;j++)
-        printf("%d",A[i]);
+    for(i=0;i<size;i++)
+        printf("%d ",A[i]);
     
     // sort Array 2 in Decending order
-    for(r=0;r<size;j++){
+    for(r=0;r<size;r++){
         for(j=0;j<size-1-r;j++)
         {
             if(B[j]<B[j+1]){
@@ -56,15 +59,27 @@ void MergeArray(int A[],int B[],int size,int merge[])
         }
     }
     
-    printf("Array 2 in Decending order:");
+    printf("\nArray 2 in Decending order:");
     for(j=0;j<size;j++)
-        printf("%d",B[j]);
-     
-    if(A[i]>A[j]){
-        merge[i]=A[i];
-        i++;
+        printf("%d ",B[j]);
+    
+    i=0,j=0,k=0;
+    while(k<size*2)
+    {
+        if(A[i]>B[j])
+        {
+            merge[k]=A[i];
+            i++;
+        }
+        else    
+        {
+            merge[k]=B[j];
+            j++;
+        }
+        k++;
     }
-    printf("Merge the array:");
-    for(i=0;i<size*2,i++)
-        merge[i]=A[i];
+
+    printf("\nArray After Merging & sorted in descending order ");
+        for(k=0;k<size*2;k++)
+            printf("%d ",merge[k]);
 }
