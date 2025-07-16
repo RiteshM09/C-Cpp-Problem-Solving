e66f7c97e447ee6abb69269803f916a85c82f0aa
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-07-16 17:41:24 +0530

    Print_UniqueElements

--- C-Cpp-Problem-Solving/c-file/Arrays/Array&Function/Assignment-sol/Print_UniqueElements.c
+++ C-Cpp-Problem-Solving/c-file/Arrays/Array&Function/Assignment-sol/Print_UniqueElements.c
@@ -3,8 +3,50 @@
 // output: 10 10 20 20 20 30 30 40
 
 // output: 40
+#include<stdio.h>
+void Print_unique(int A[],int);
+int main()
+{
+    int s,a[20],i;
+    printf("Enter Array size: ");
+    scanf("%d",&s);
+
+    printf("Enter Array elements: ");
+    for(i=0;i<s;i++)
+        scanf("%d",&a[i]);
+
+    Print_unique(a,s);
+    return 0;
+}
 void Print_unique(int A[],int size){
-    int i;
-    
-    
+    int i,temp;
+    for(int r=0;r<size;r++){
+        for(i=0;i<size-1-r;i++){
+            if(A[i]>=A[i+1])
+                {
+                    temp=A[i];
+                    A[i]=A[i+1];
+                    A[i+1]=temp;
+                }
+        }
+    }
+    printf("Sorted array: ");
+        for(i=0;i<size;i++)
+            printf("%d ",A[i]);
+
+    printf("\nUnique items are:");
+    i=0;
+    if(A[i]!=A[i+1])
+        printf("%d ",A[i]);
+    i=1;
+    while(i<=size-2){
+        if(A[i]!=A[i+1] && A[i]!=A[i-1])
+            printf("%d ",A[i]);
+            
+        i++;
+
+    }
+    if( A[size-1]!=A[size-2])
+        printf(" %d",A[size-1]);
+
 }
