800aa804e08eaeda249ae069d84097c850809e5b
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-07-17 18:19:40 +0530

    solution 2 find product of 3*3 matrix added

---
+++ C-Cpp-Problem-Solving/c-file/Arrays/Multi-Dimensional-Arrays/Assignment-sol/Productof2Matrices3x3.c
@@ -0,0 +1,55 @@
+//Write a program to calculate the product of two matrices each of order 3x3.
+// Write a program to calculate the sum of two matrices each of order 3x3.
+// input - 
+#include<stdio.h>
+int main()
+{
+    int a[3][3],b[3][3],i,j,product[3][3];
+    printf("Enter 1st array: element ");
+    for(int i=0;i<=2;i++)
+    {
+        for(int j=0;j<=2;j++)
+            scanf("%d",&a[i][j]);
+        printf("\n");
+    }
+    printf("Enter 2nd array: element");
+    for(int i=0;i<=2;i++)
+    {
+        for(int j=0;j<=2;j++)
+            scanf("%d",&a[i][j]);
+        printf("\n");
+    }
+
+    printf("\nPrint 1st array element \n");
+    for(int i=0;i<=2;i++)
+    {
+        for(int j=0;j<=2;j++)
+            printf("%d ",a[i][j]);
+        printf("\n");
+    }
+    printf("\nPrint 2nd array element\n");
+    for(int i=0;i<=2;i++)
+    {
+        for(int j=0;j<=2;j++)
+            printf("%d ",a[i][j]);
+        printf("\n");
+    }
+
+    printf("\nProduct of 2 array of order 3*3 :\n");
+    for(i=0;i<=2;i++)
+    {
+        for(j=0;j<=2;j++)
+            product[i][j]=a[i][j]*a[i][j];
+    }
+
+    for(i=0;i<=2;i++)
+    {
+        for(j=0;j<=2;j++)
+            printf("%d ",product[i][j]);
+        printf("\n");
+    }
+
+    
+
+    return 0;
+}
