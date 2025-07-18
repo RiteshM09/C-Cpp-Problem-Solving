ebec41e664936317e5147501e1a2c7d9568bbb0b
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-07-18 17:59:06 +0530

    modified code

--- C-Cpp-Problem-Solving/c-file/Arrays/Multi-Dimensional-Arrays/Assignment-sol/Productof2Matrices3x3.c
+++ C-Cpp-Problem-Solving/c-file/Arrays/Multi-Dimensional-Arrays/Assignment-sol/Productof2Matrices3x3.c
@@ -4,7 +4,7 @@
 #include<stdio.h>
 int main()
 {
-    int a[3][3],b[3][3],i,j,product[3][3];
+    int a[3][3],b[3][3],i,j,product[3][3],sum,k;
     printf("Enter 1st array: element ");
     for(int i=0;i<=2;i++)
     {
@@ -16,7 +16,7 @@ int main()
     for(int i=0;i<=2;i++)
     {
         for(int j=0;j<=2;j++)
-            scanf("%d",&a[i][j]);
+            scanf("%d",&b[i][j]);
         printf("\n");
     }
 
@@ -31,7 +31,7 @@ int main()
     for(int i=0;i<=2;i++)
     {
         for(int j=0;j<=2;j++)
-            printf("%d ",a[i][j]);
+            printf("%d ",b[i][j]);
         printf("\n");
     }
 
@@ -39,7 +39,11 @@ int main()
     for(i=0;i<=2;i++)
     {
         for(j=0;j<=2;j++)
-            product[i][j]=a[i][j]*a[i][j];
+        {
+            for(k=0,sum=0;k<=2;k++)
+                sum = sum + (a[i][k]*b[k][j]); 
+            product[i][j] = sum;
+        }
     }
 
     for(i=0;i<=2;i++)
@@ -48,8 +52,6 @@ int main()
             printf("%d ",product[i][j]);
         printf("\n");
     }
-
-    
 
     return 0;
 }
