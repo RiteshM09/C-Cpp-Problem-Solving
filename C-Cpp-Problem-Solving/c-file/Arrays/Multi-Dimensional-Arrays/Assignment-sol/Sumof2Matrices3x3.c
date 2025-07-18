ca916ae8e63c7fde05a356ec35053cc6ac6c349b
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-07-18 17:59:34 +0530

    resolved errors

--- C-Cpp-Problem-Solving/c-file/Arrays/Multi-Dimensional-Arrays/Assignment-sol/Sumof2Matrices3x3.c
+++ C-Cpp-Problem-Solving/c-file/Arrays/Multi-Dimensional-Arrays/Assignment-sol/Sumof2Matrices3x3.c
@@ -15,7 +15,7 @@ int main()
     for(int i=0;i<=2;i++)
     {
         for(int j=0;j<=2;j++)
-            scanf("%d",&a[i][j]);
+            scanf("%d",&b[i][j]);
         printf("\n");
     }
 
@@ -30,7 +30,7 @@ int main()
     for(int i=0;i<=2;i++)
     {
         for(int j=0;j<=2;j++)
-            printf("%d ",a[i][j]);
+            printf("%d ",b[i][j]);
         printf("\n");
     }
 
@@ -38,7 +38,7 @@ int main()
     for(i=0;i<=2;i++)
     {
         for(j=0;j<=2;j++)
-            sum[i][j]=a[i][j]+a[i][j];
+            sum[i][j]=a[i][j]+b[i][j];
     }
 
     for(i=0;i<=2;i++)
