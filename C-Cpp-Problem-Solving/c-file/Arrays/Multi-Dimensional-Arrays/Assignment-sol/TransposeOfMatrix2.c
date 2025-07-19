e93943f62668942e592f943424055e1ef1f7c327
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-07-19 16:27:03 +0530

    code updated

--- C-Cpp-Problem-Solving/c-file/Arrays/Multi-Dimensional-Arrays/Assignment-sol/TransposeOfMatrix2.c
+++ C-Cpp-Problem-Solving/c-file/Arrays/Multi-Dimensional-Arrays/Assignment-sol/TransposeOfMatrix2.c
@@ -4,25 +4,27 @@
 #include<stdio.h>
 int main()
 {
-    int a[10][10],i,j,t[10][10];
+    int a[10][10],i,j,t[10][10],row, col;
+    printf("Enter no. of Rows and columns:");
+    scanf("%d %d",&row,&col);
     printf("Enter numbers for matrix:");
-    for(i=0;i<=2;i++)
+    for(i=0;i<row;i++)
     {
-        for(j=0;j<=2;j++)
+        for(j=0;j<col;j++)
             scanf("%d",&a[i][j]);
         printf("\n");
     }
-    printf("Transpose of  matrix element ");
-    for(i=0;i<=2;i++)
+    printf("Transpose of  matrix element: \n");
+    for(i=0;i<row;i++)
     {
-        for(j=0;j<=2;j++){ //01 10 02 20 12 21  
+        for(j=0;j<col;j++){ //01 10 02 20 12 21  
             t[j][i]=a[i][j];
         }
     }
 
-    for(i=0;i<=2;i++)
+    for(i=0;i<col;i++)
     {
-        for(j=0;j<=2;j++)
+        for(j=0;j<row;j++)
             printf("%d ",t[i][j]);
         printf("\n");
     }
