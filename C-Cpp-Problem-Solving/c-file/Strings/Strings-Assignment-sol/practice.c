4efd8609b523fe45807b7ae2087f77e78a8c03e5
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-07-19 19:03:11 +0530

    String folder added

---
+++ C-Cpp-Problem-Solving/c-file/Strings/Strings-Assignment-sol/practice.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+                // 0   1   2   3   4   5   0  0  0  0
+    char str[5]="RITESH"; // string array 
+    
+    
+    printf("Enter Name:");
+    gets(str);
+
+    printf("%s",str);
+    return 0;
+}
