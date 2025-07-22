64c3acdd7dd38c668f4af25e76912c9d8824f909
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-07-22 17:25:40 +0530

    name change

---
+++ C-Cpp-Problem-Solving/c-file/Strings/Strings-Assignment-sol/FuncLastCharofString.c
@@ -0,0 +1,25 @@
+// funtion to return last character of a string.
+// Ritesh
+// h
+#include<stdio.h>
+#include<string.h>
+int LastCharacter(char s[]);
+int main()
+{
+                // 0   1   2   3   4   5   0  0  0  0
+    char ch,str[20];// string array 
+    int i;
+    printf("Enter name: ");
+    fgets(str,20,stdin);
+
+    str[strlen(str)-1]='\0';// 
+    ch = LastCharacter(str); 
+    //  for(i=0;str[i];i++);
+    printf("Print last character: %c",ch);
+    printf("\n");
+    return 0;
+}
+int LastCharacter(char s[])//ex: length(Ritesh) = 0123456
+{
+    return s[strlen(s)-1];// ex: s[6-1] - s[5] - 
+}   
