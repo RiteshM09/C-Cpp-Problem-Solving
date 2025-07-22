6b0750f908ae30b9420bbc1f3a71391c1174a07d
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-07-22 15:47:21 +0530

    new file added

--- C-Cpp-Problem-Solving/c-file/Strings/Strings-Assignment-sol/practice.c
+++ C-Cpp-Problem-Solving/c-file/Strings/Strings-Assignment-sol/practice.c
@@ -1,13 +1,19 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
                 // 0   1   2   3   4   5   0  0  0  0
-    char str[5]="RITESH"; // string array 
-    
-    
-    printf("Enter Name:");
-    gets(str);
+    char str[20];// string array 
+    int i;
+    printf("Enter name: ");
+    fgets(str,20,stdin);
 
-    printf("%s",str);
+    printf("%s ",str);
+    // str[strlen(str)-1]='\0';
+
+    // for(i=0;str[i];i++);
+
+    // printf("\nlength of string : %d",i);
+    printf("\n");
     return 0;
 }
