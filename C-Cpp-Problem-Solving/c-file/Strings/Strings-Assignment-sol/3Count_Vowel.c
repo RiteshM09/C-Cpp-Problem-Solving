626b708c07b8a3ac58c286cac2952540efe2ac4e
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-07-23 00:14:12 +0530

    sol 3 count vowel in a given string

--- C-Cpp-Problem-Solving/c-file/Strings/Strings-Assignment-sol/3Count_Vowel.c
+++ C-Cpp-Problem-Solving/c-file/Strings/Strings-Assignment-sol/3Count_Vowel.c
@@ -1,9 +1,29 @@
 //Write a program to count vowels in a given string
-// input: Programming
+// input: hello world
 // output: 3 vowels
 #include<stdio.h>
+#include<string.h>
 int main()
 {
-    
+    char str[50],v[]="aeiouAEIOU";
+    int count=0,i,j;
+
+    printf("Enter a String: ");
+    fgets(str,50,stdin);
+
+    str[strlen(str)-1]='\0';
+
+    for(i=0;str[i];i++)   // rit 
+        for(j=0;v[j];j++) // aeiouAEIOU
+            if(v[j]==str[i])
+            {
+                count++;
+                break; // if vowel found, break & move to next letter
+            }
+
+
+    printf("%d vowels",count);
+
+    printf("\n");
     return 0;
 }
