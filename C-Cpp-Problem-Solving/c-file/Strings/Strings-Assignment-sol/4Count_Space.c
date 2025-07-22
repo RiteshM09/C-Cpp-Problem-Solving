4d1cbe8f62e9ef8d110e82b625c16e3ed7e45742
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-07-23 00:47:05 +0530

    description added to avoid errors

--- C-Cpp-Problem-Solving/c-file/Strings/Strings-Assignment-sol/4Count_Space.c
+++ C-Cpp-Problem-Solving/c-file/Strings/Strings-Assignment-sol/4Count_Space.c
@@ -12,15 +12,18 @@ int main()
     fgets(str,50,stdin);
 
     str[strlen(str)-1]='\0';
-
+   
     for(i=0,count=0;i<=str[i];i++) 
         if(str[i]==space)
             count++;
-    
+
     printf("%d space's ",count);
 
     return 0;
 }
+// use ch=' ' to compare otherwise waring is generated: 
+// u r trying to assign a string (char array) to a single character variable (char), 
+// which is not allowed in C.(ch = " ") use single cote for character and double for multiple character 
 
 
 
