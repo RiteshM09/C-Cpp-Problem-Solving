76f767931c896c5b5d9f843324bb67c65ed2e57f
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-07-30 18:20:19 +0530

    string and pointer

---
+++ C-Cpp-Problem-Solving/c-file/Pointers/practice2.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<string.h>
+int length(char *p);
+int main()
+{
+    int l;
+    char str[10];
+
+    printf("Enter string: ");
+    fgets(str,10,stdin);
+    str[strlen(str)-1]='\0';
+
+    l=length(str);
+    printf(" lenght: %d",l);
+    return 0;
+}
+
+int length(char *p)
+{
+    int i;
+    for(i=0;*(p+i);i++);
+    return i;
+}
+
+// *p     - a[0];
+// *(p+i) - a[i];
+
+// p - &a
+// p+1 - &a[1]
+// p+i - &a[i]
