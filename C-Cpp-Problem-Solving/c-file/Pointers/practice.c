5465cca74dcd0b8b6b2310e09b8054ef5635f5db
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-07-30 17:44:43 +0530

    pointer and array

--- C-Cpp-Problem-Solving/c-file/Pointers/practice.c
+++ C-Cpp-Problem-Solving/c-file/Pointers/practice.c
@@ -1,23 +1,39 @@
 // x=5, xaddress=1000, j=1000, jaddress=2000
 #include <stdio.h>
 void swap(int*, int*);
-int input(int a[],int);
+void input(int*, int );
+void display(int*,int);
 int main()
 {
-    int a,b;
-    printf("Enter 2 number : ");
-    scanf("%d %d",&a,&b);
-    swap(&a,&b);
-    printf("a=%d,b=%d",a,b);
-
+     int a[10],i;
+    // printf("Enter 2 number : ");
+    // scanf("%d %d",&a,&b);
+    // swap(&a,&b);
+    // printf("a=%d,b=%d",a,b);
+    input(a,10);
+    display(a,10);
     return 0;
 }
+void input(int *p,int size){
+    int i;
+    printf("Enter size: %d :",size);
+    for(i=0;i<size;i++)
+        scanf("%d",p+i);// p+i = &a[i]
+}
+void display(int *p,int size){
+    int i;
+
+    for(i=0;i<size;i++)
+        printf("%d ",*(p+i));
+}
+
 void swap(int *p,int *q){ 
     int temp;
     temp=*p;
     *p=*q;
     *q=temp; 
 }
+
 
 // int input(int a[],int){
 // }
