9838546ec8fbdea2d510b295ec5c4f379cf647b1
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-08-03 15:42:05 +0530

    sol3 compare 2 string

--- C-Cpp-Problem-Solving/c-file/Pointers/Assignment-sol/2.c
+++
@@ -1,6 +0,0 @@
-/*Write a function to search all occurrences of a 
-given character in a given string.
-Result of search is a list of indices 
-to be stored in the given array. 
-[ void search_all _occurrences(char *str, char ch, int *arr); 1
-*/
