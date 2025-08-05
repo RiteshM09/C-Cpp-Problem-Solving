1776f9fdc30401d25ebe744cf9e5300dee5df9ba
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-08-05 17:23:25 +0530

    sol 10 function to check string is alpha numerical or not

---
+++ C-Cpp-Problem-Solving/c-file/Strings/String&Function/Assignment-sol-String&Funct/10FunToCheckStingAlphanumeric.c
@@ -0,0 +1,52 @@
+//Write a function to check whether a given string is an alphanumeric string or not.
+//(Alphanumeric string must contain at least one alphabet and one digit)
+/*
+1 input: Hello
+  Output: No
+
+2 input: Hello123
+  Output: Yes
+
+3 input: Hello@123
+  Output: No
+*/
+#include<stdio.h>
+#include<string.h>
+int Check_Alphanumeric(char s[]);
+int main()
+{
+    char str[50];
+    printf("Enter String: ");
+    fgets(str,50,stdin);
+    str[strlen(str)-1]='\0';
+
+    if(Check_Alphanumeric(str))
+        printf("True");
+    else    
+        printf("False");
+
+    return 0;
+
+}
+int Check_Alphanumeric(char s[])
+{
+    int i,HasAlpha=0,HasDigits=0;
+    for(i=0;s[i];i++)
+    {
+        if((s[i]>='a' && s[i]<='z') || (s[i]>='A' && s[i]<='Z')){
+            HasAlpha=1;
+        }
+        else if(s[i]>='1' && s[i]<='9'){
+            HasDigits=1;
+        }
+        else{
+            return 0;      
+        } 
+           
+    }
+    if(HasAlpha==1 && HasDigits==1)   
+        return 1;
+    else
+        return 0;
+}
+
