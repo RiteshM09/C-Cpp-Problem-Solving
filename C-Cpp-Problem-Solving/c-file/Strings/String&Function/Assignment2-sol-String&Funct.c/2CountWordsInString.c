e557bbbd8096b83575f62391f5566b01927f196e
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-08-10 17:09:30 +0530

    code updated

---
+++ C-Cpp-Problem-Solving/c-file/Strings/String&Function/Assignment2-sol-String&Funct.c/2CountWordsInString.c
@@ -0,0 +1,51 @@
+// Write a function to count words in a given string
+/*
+input: Hello World
+output: 2
+
+input: ""  
+input: " "
+output: 0
+
+input: all is well
+output: 3
+
+input: A 
+output: 1
+*/
+#include<stdio.h>
+#include<string.h>
+int CountWords(char s[]);
+int main()
+{
+    char str[50];
+    printf("Enter a string:");
+    fgets(str,50,stdin);
+    str[strlen(str)-1]='\0';  
+    
+    printf("Total words:%d",CountWords(str));
+    return 0;  
+}
+int CountWords(char s[])
+{      
+    int i=0,count=0,flag=0;
+    
+    while(s[i])
+    {
+        flag=0;// reinitilize flag for next word
+        while(s[i]!=' ' && s[i]) // check s[i] is non space and inside the string
+        {
+            flag++;
+            i++;
+        }
+
+        if(flag) // if flag is non zero then increase count
+            count++;
+
+        i++;// move to next character
+    }
+    return count;
+}
+
+
+ 
