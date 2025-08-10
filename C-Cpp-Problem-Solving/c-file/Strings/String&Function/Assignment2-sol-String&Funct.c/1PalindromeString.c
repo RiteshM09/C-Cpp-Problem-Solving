e0663419f2959deaac6bcf62675567d60997a168
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-08-10 16:00:39 +0530

    file name corrected

---
+++ C-Cpp-Problem-Solving/c-file/Strings/String&Function/Assignment2-sol-String&Funct.c/1PalindromeString.c
@@ -0,0 +1,50 @@
+//1. Write a function to check whether a given string is palindrome or not.
+/*
+input : Hello
+output: No
+
+input : TNT
+output: yes
+
+input : Madam
+output: Not 
+
+*/
+#include<stdio.h>
+#include<string.h>
+int PalindromeString(char s[]);
+int main()
+{
+    char str[50];
+    printf("Enter a string: ");
+    fgets(str,50,stdin);
+    str[strlen(str)-1]='\0';    
+   
+    if(PalindromeString(str))
+        printf("TRUE");
+    else    
+        printf("FALSE");
+
+}
+int PalindromeString(char s[])
+{   
+    char original[50],ch;
+    int i,len;
+    strcpy(original,s);
+
+    len=strlen(s);
+    // Reverse string
+    for(i=0;i<len/2;i++) // 0 to len/2 
+        if(s[i]==s[len-1-i])//
+            return 1;
+    return 0;
+}
+
+ 
+// while(s[i])TNT
+// if(s[i])
+// i--;
+
+// madam
+// 1 rev sting
+// if(s[i]==original)
