6cbc07fc49d9dce958690ba7ec473e6aa3d94de5
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-11-10 17:20:36 +0530

    descending order BuBBle sort logic improved

--- C-Cpp-Problem-Solving/c-file/Arrays/Array-Assignment-solutions/9SortArrayinDecending.c
+++ C-Cpp-Problem-Solving/c-file/Arrays/Array-Assignment-solutions/9SortArrayinDecending.c
@@ -1,18 +1,21 @@
 //Write a program to sort an array of 10 elements in descending order.
 // i\p -  50 20 30 80 10 40 60 90 100 15
 // o/p - 100 90 80 60 50 40 30 20 15 10
+// descending order bubble sort
 #include<stdio.h>
 int main()
 {
-    int i,a[10],r,t;
-    printf("Enter 10 values : ");
-    for(i=0;i<=9;i++)
+    int i,a[10],r,t,size;
+    printf("Enter size of array: ");
+    scanf("%d",&size);
+    printf("Enter n values : ");
+    for(i=0;i<size;i++)
         scanf("%d",&a[i]);
 
-    for(r=1;r<=9;r++){
-     for(i=0;i<=9-r;i++)
-     { 
-        if(a[i]<a[i+1])
+    for(r=1;r<size;r++){// if size=10,then 9 rounds(1 to size-1)
+     for(i=0;i<size-r;i++)// for rounds1, r=1: i runs from 0to8 (size - 1 - r => 9(10-1) - 1 = 8), smallest element moves to end
+     {                    // for r=2, total:8 comparisons  0to7 (9-2=7)
+        if(a[i]<a[i+1])   
         {
          t=a[i];
          a[i]=a[i+1];
@@ -22,7 +25,7 @@ int main()
     }   
 
     printf("Sorted array in Descending Order :");
-    for(i=0;i<=9;i++)
+    for(i=0;i<=size-1;i++)
         printf("%d ",a[i]);
     
 
