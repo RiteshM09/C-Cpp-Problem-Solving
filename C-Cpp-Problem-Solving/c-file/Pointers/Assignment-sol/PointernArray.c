019ac788779210b61a4338dc151532a5a9a5e074
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-11-27 17:22:29 +0530

    input and display fun using array

--- C-Cpp-Problem-Solving/c-file/Pointers/Assignment-sol/PointernArray.c
+++ C-Cpp-Problem-Solving/c-file/Pointers/Assignment-sol/PointernArray.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 void input(int* ,int);
+void display(int*,int);
 int main()
 {
     int a[10];
     printf("Enter 10 valuse: ");
     input(a,10);
+    display(a,10);
     printf("\n");
     return 0;
 }
@@ -19,4 +21,9 @@ void input(int *p,int size){
         // p+3 --> &a[3]
         // p+4 --> &a[4]
         // p+5 --> &a[5].....
+}
+void display(int *p,int size)
+{
+    for(int i=0;i<size;i++)
+        printf("%d ",*(p+i)); // *(p+0) ~ derefecencing at p+0 
 }
