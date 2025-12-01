520b4c48c1560f7cb84aa067e2831f1b39441efd
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-12-01 16:58:56 +0530

    pointer program

---
+++ C-Cpp-Problem-Solving/c-file/Pointers/Practice/PointerANDstring.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int length(char *);
+int main()
+{
+    int l;
+    char str[10];
+    fgets(str,10,stdin);
+    l=length(str);
+    printf("lenght %d ",l);
+    return 0;
+}
+int length(char *p)
+{
+    int i;
+    for(i=0;*(p+i);i++);
+    return i;
+}
