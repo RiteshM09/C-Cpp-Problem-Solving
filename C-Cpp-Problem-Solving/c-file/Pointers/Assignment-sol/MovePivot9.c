38b2f199c02bfebf5b4d921fee7309a02d095104
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2025-12-10 15:25:35 +0530

    progam 9 added

--- C-Cpp-Problem-Solving/c-file/Pointers/Assignment-sol/MovePivot9.c
+++ C-Cpp-Problem-Solving/c-file/Pointers/Assignment-sol/MovePivot9.c
@@ -3,12 +3,12 @@
 // in the left and greater values will be in the right.
 /*
 
-input : arr = [23, 67, 11, 35, 42, 59, 16, 8, 81]
-Output: arr = [11, 16, 8 ,23, 67, 35, 42, 59, 81]
+input : arr = [23, 67, 89, 11, 35, 42, 59, 16, 8 , 81]
+Output: arr = [8 , 11, 16, 23, 35, 42, 59, 89, 67, 81]
         pivot final index = 3
 
-input : arr = [20, 10, 5, 30, 25]
-Output: arr = [10, 5, 20, 30, 25]
+input : arr = [20, 10,  5, 30, 25]
+Output: arr = [ 5, 10, 20, 30, 25]
         pivot final index = 2 
 
 input : arr = [3, 1, 2]
@@ -25,26 +25,34 @@ void swap(int *,int *);
 int MoveValue(int*,int);
 int main()
 {
-    int Arr[]={20,10,5,30,25};
+    int Arr[]={23, 67, 89, 11, 35, 42, 59, 16, 8 , 81};
     int size;
     size = sizeof(Arr) / sizeof(Arr[0]);
+    
     MoveValue(Arr,size);
 
-    printf("New array: ");
+    printf("\nNew array: ");
     for(int i=0;i<size;i++)
         printf("%d ",Arr[i]);
+    
 }
 int MoveValue(int *A,int n){
-    int pivot=0,left, right=n-1;
+    int i,pivot=0,left=1;
     // greater val is in right
 
-    while(left<right)
-    {
-        while(){
-           
+
+
+    for(i=1;i<n;i++){
+        if(A[i]<A[pivot]){// if read pointer(i) is smaller than pivot, then swap left with read (i)pointer
+                swap(&A[i],&A[left]);
+                left++;
         }
     }
-    
+    //now after loop ends left pointer is at element 
+    //which is greater than pivot
+    //so swap its previous value(left-1 index)
+    swap(&A[pivot],&A[left-1]);
+    printf("Pivot is at index: %d",left-1);
     return *A;
 }
 void swap(int *x,int *y)
