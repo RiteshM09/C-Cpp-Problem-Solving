400ff80459973abab05f7a9a42a4d72e362e4d06
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2026-01-10 17:51:11 +0530

    add que 4 5 of chess problem

--- C-Cpp-Problem-Solving/c-file/aREVISION/Array2practice/2D_Array.c/Chess.c
+++ C-Cpp-Problem-Solving/c-file/aREVISION/Array2practice/2D_Array.c/Chess.c
@@ -6,8 +6,9 @@
 
 // 1) define a function to update score_board after each match result.
 // 2) define a function to display score_ _board. 
-/*
-
+/* 3) define a function which returns the score of a specific player
+   4) define a function to find the winner of the tournament.
+   5) define a function to display rank of the players.
 total matches : matches 6
 p0, p1, p2, p3
 
@@ -22,7 +23,10 @@ p2 vs p3 -
 #include<stdio.h>
 void update_Score_board(int a,int b,int result,int score_board[4][4]);
 void input();
+int winner(int score_board[4][4]);
+int get_player_score(int player,int score_board[4][4]);
 void display_score_board(int score_board[4][4]);
+void Player_Ranking(int score_board[4][4]);
 void input()
 {
     int score_board[4][4]={0}; // initially all val player val are 0
@@ -44,6 +48,19 @@ void input()
 
     printf("\n");
     display_score_board(score_board);
+
+    printf("\n");
+    for(int i=0;i<4;i++)
+        printf("Player %d has total points = %d\n", i, get_player_score(i, score_board));
+
+    //display winner 
+    printf("\n WINNER of tournament is : player[%d]", winner(score_board));
+
+    printf("\n Player Ranking :");
+    Player_Ranking(score_board);
+
+    printf("\n");
+    
 }
 int main()
 {
@@ -89,4 +106,92 @@ void display_score_board(int score_board[4][4])
         }
         printf("\n");
     }
+}
+int get_player_score(int player,int score_board[4][4]){
+    // player’s points stored In that player’s ROW of the score_board matrix 
+    int total=0;
+
+     for(int i = 0; i < 4; i++)
+     {
+        
+        total = total + score_board[player][i];
+     }
+     return total;
+    printf("\n");
+}
+int winner(int score_board[4][4])
+{       
+   
+    int player=0, player_score = 0;
+    int existing_score = get_player_score(player,score_board);// 1st player score  // pass player 0 value in existing score 
+    // now check if next players(i.e p1..)  points are greater, greater points goes in winner variable 
+    int winner = player ;
+    for(int player=1 ; player< 4 ; player++)
+    {
+        player_score = get_player_score(player, score_board);
+        if( player_score > existing_score )  // if next player has more points 
+        {
+              winner = player; // change the winner val to current player ( with greater score )
+              existing_score = player_score; // & update the old score to greater player score
+        }
+          
+    }
+
+    return winner;
+
+}
+void sort(int [],int,int []);
+void sort(int ar[],int n,int index[])
+{
+    int i,j,temp;
+
+    for(i=0;i<n;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(ar[j]<ar[j+1])
+            {
+                temp=ar[j]; 
+                ar[j]=ar[j+1];
+                ar[j+1]=temp;
+
+            // also swap the index
+                int temp2=index[j];
+                index[j]=index[j+1];
+                index[j+1]=temp2;
+            }
+        }
+    }
+}
+void Player_Ranking(int score_board[4][4]){
+    // sort player in descending order point wise
+    // 1) player[0] -> 5 points
+    // 2) player[3] -> 4 points
+    // 3) player[2] -> 2 points
+    // 4) player[3] -> 1 points 
+
+    int score[4];
+    int player[4];
+    int player_index[4];
+
+    for(int p=0;p<4;p++){
+        score[p]  = get_player_score(p, score_board);
+        player[p] = score[p];
+        player_index[p] = p;
+    }      
+    // in player[4] array we have 0 1 2 3 -> 5 1 2 4
+    // &in score[4] array we have 0 1 2 3 -> 5 1 2 4 then we sort it
+    // score[4] becomes  0 1 2 3 -> 5 4 2 1
+
+    sort(score,4,player_index); // i.e, score array is sorted we have 5,4,2,1 
+    printf("\n");
+    for(int i=0;i<4;i++)
+    {
+          printf("%d). Player[%d] -> %d points ",i+1, player_index[i] , score[i]);
+          printf("\n");
+    }
+      
+    
+
+
 }
