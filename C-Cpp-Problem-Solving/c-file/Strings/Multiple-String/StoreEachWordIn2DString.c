45980db075e62821f740a79bc19e4d4d6a811e47
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2026-01-13 13:52:43 +0530

    corrected code for 1d to 2d string

--- C-Cpp-Problem-Solving/c-file/Strings/Multiple-String/StoreEachWordIn2DString.c
+++ C-Cpp-Problem-Solving/c-file/Strings/Multiple-String/StoreEachWordIn2DString.c
@@ -10,49 +10,132 @@ output 2d string : s[0][20] = hello
 */
 #include<stdio.h>
 #include<string.h>
-void StoreStringWord(char [], char word[5][20]);
+#define MAX 100
+int coutwords(char str[MAX]);
+void TrimSpaces(char str[MAX]);
+void StoreStringWord(char [MAX], char word[20][MAX]);
 int main()
 {
-    char s[20];
-    char w[5][20];
+    char s[MAX];
+    char w[20][MAX];
     printf("enter a string : ");
-    fgets(s,20,stdin);
+    fgets(s,MAX,stdin);
     s[strlen(s)-1]='\0';
-
+    TrimSpaces(s);
     StoreStringWord(s,w);
+    
+    
     return 0;
 }
-void StoreStringWord(char str[20], char word[5][20]){// hell0 
-    // char str[size][20];
-    // for(int i=0;original[i];i++)
-    // {
-    //     strcpy(str[i],original[i]);
-    // }
-
-    //  for(int i=0;str[i];i++)
-    //     printf(" %s",str[i]);
- 
-}
-int coutwords(char str[20])
-{
+void StoreStringWord(char str[MAX], char word[20][MAX]){// hell0 
+    // string :: "hello 
+    //this 
+    //is 
+    //ritesh"
+   
     int i=0;
-    count=0;
-    // skip leading spaces 
-    while(str[i])
-    i++;
+    while (str[i] == ' ')
+            i++;
 
-    // i is at non spacing char
+    int count = coutwords(str);
+
+    // word[0][20] = str[0,1,2,3,4,\0]
+    // word[1][20] = str[0,1,2,3,4,\0]
+    
+    // word[j][k++] = str[i++];
+
+    int j=0,k=0;
+    for(i=0;str[i];i++,k++)
+    {
+        if(str[i]!=' ')
+        {
+            word[j][k]=str[i];
+
+        }
+        else if(str[i]==' ')
+        {
+            word[j][k]='\0';
+            j++;
+            // handling overflow of string 
+            if (j >= count) break; // stop j when greater than 5, i.e 0,1,2,3,4
+            
+            k=-1; // as k will increment after this condition soo set k=-1, so next becomes k=0
+        }
+        
+    }
+    
+    // at last position
+    if(j<count)
+    word[j][k]='\0';
+
+    for(int d=0;d<count;d++)
+        printf("%s\n",word[d]);
+}
+int coutwords(char str[MAX])
+{
+    
+    int i=0;
+    int count=0,inword=0;
+
+    // "hello this is ritesh"
 
     while(str[i]!='\0')
     {
-        if(str[i]==' ')
+        if(str[i]!=' ' && inword==0 )// detect start of word
         {
-            count=1;
-            break;
+            count++;
+            inword=1;
         }
-        i++;
+        else if(str[i]==' ')
+        {
+            inword=0;
+        }
+      i++;
     }
-    // i is at 1 spacing char
-    while(str[i]= str[i]!='\0')
-        
+   
+    return count;
+}
+void TrimSpaces(char str[MAX]){
+    // if (str[i] == '\0')
+    // {
+    // str[0] = '\0';
+    //  return 0;
+    // }
+
+    //starting 
+    int i=0;
+    while(str[i]==' ')
+        i++;
+
+    // leading 
+    int len=strlen(str);
+    for(int j=len-1;j>i;j--) //not j should be greater than i 
+    {
+        if(str[j]!=' ')
+        {
+              str[j+1]='\0';// place null after non-spacing char
+              break;
+        }
+    }
+    // i is at starting non space char;
+    // j is at leading  non space char;
+    int write=i;
+    while(str[i]!='\0')
+    {
+        if(str[i]!=' ') // if its a non space keep it
+        {
+            str[write]=str[i];
+            write++;
+            i++;
+        }
+        else if(str[i]==' ' && str[i-1]!=' ') // if its space and prev index is non space keep it
+        { 
+            str[write]=str[i];
+            write++;
+            i++;
+        }     
+        else
+            i++; 
+    }
+    str[write]='\0';
 }
