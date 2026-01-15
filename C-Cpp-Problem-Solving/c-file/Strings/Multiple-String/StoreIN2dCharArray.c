790e32f071ab8f67be1ba92db9b6cf9fa4c1eac5
Ritesh Mendhekar <riteshmendhekar09@gmail.com>
2026-01-15 23:54:25 +0530

    code fixed

--- C-Cpp-Problem-Solving/c-file/Strings/Multiple-String/StoreIN2dCharArray.c
+++ C-Cpp-Problem-Solving/c-file/Strings/Multiple-String/StoreIN2dCharArray.c
@@ -17,14 +17,18 @@ int main()
     fflush(stdin);
     storeIn2d(str,n);
 
+   
+    for(int j=0;j<n;j++)   
+        printf("%s\n",str[j]);
+
     return 0;
 }
 void storeIn2d(char str[][MAX],int size){
-
+    
      for(int i=0;i<size;i++)
      {
             printf("Enter Name #%d : ",i+1);
             fgets(str[i],MAX,stdin);  
-    
+            str[i][strlen(str[i])-1]='\0';
      }
 }
